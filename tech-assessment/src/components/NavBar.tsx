91fe79c03721d5143c433882b70ed9cd04137480
Joel Danielz <joeldanielz@icloud.com>
2024-11-01 14:59:06 +0000

    Adding logo

--- tech-assessment/src/components/NavBar.tsx
+++ tech-assessment/src/components/NavBar.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { AppBar, Box, IconButton } from '@mui/material';
 import MenuIcon from "@mui/icons-material/Menu";
-import React from 'react';
+import logo from "../assets/images/logo.png"
+import { Image } from '@mui/icons-material';
 
 interface Props {
   setIsHamburgerOpen: (isHamburgerOpen: boolean) => void;
@@ -53,8 +55,8 @@ export default function NavBar(props: Props) {
             >
               <MenuIcon />
             </IconButton>
-            <a href="/" style={{ textDecoration: "none", color: "white" }}>
-              <h1>Logistics UK</h1>
+            <a href="/" style={{ textDecoration: "none", color: "white", marginTop:"1.5rem", marginBottom:"1.5rem" }}>
+              <img src={logo} />
             </a>
           </Box>
           <Box
